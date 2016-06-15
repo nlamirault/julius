5d22396adbb1e9dd85468e3d236fc71ace010b0c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-06-16 00:33:30 +0200

    Connect to an IRC server and init commands
    Init command : help, version
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+extern crate irc;
+
+use std::default::Default;
+use irc::client::prelude::*;
+
+mod version;
+
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config {
+        nickname: Some(format!("juliusbot")),
+        alt_nicks: Some(vec![format!("juliusbot_"), format!("juliusbot__")]),
+        server: Some(format!("irc.freenode.net")),
+        channels: Some(vec![format!("#perave")]),
+        .. Default::default()
+    };
+    let server = IrcServer::from_config(config).unwrap();
+    server.identify().unwrap();
+    for message in server.iter() {
+        let message = message.unwrap(); // We'll just panic if there's an error.
+        print!("{}", message);
+        match message.command {
+            Command::PRIVMSG(ref target, ref msg) =>
+                if msg.contains("!version") {
+                    server.send_privmsg(target, &version::version()).unwrap();
+                } else if msg.contains("!help") {
+                    server.send_privmsg(target, "Julius -- An IRC bot").unwrap();
+                },
+            _ => (),
+        }
+    }
 }
