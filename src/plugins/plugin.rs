630cfc633258f0d03e55605fbfbed58f7cfc2d6b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-06-17 01:28:42 +0200

    Cleanup
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/plugins/plugin.rs
+++ src/plugins/plugin.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use irc::client::data::Message;
 use irc::client::server::IrcServer;
 
 pub trait Plugin {
